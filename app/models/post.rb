82090c08e22eabf2c7b30bcb09a87a636410344d
Spencer Mathews <smathews0906@gmail.com>
2022-08-11 17:54:42 -0500

    commit tree 2c78cd85ee79c6ed4ebe8e124cec2b0f36c3d496

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, exclusion: { in: %w(True Facts) }
+    validates :content, length: { minimum:250 }
+    validates :summary, length: { maximum:250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
